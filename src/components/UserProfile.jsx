96926cfabe573b22a95d3c97c238c085dc65c718
Ignacio Vinke <ignaciovinke@gmail.com>
2025-05-19 13:20:25 -0400

    Remove: Refresh Auth Token button from profile page UI

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -517,15 +517,6 @@ function UserProfile() {
           </button>
         </div>
       </form>
-
-      <div className="mt-6 flex justify-end">
-        <button 
-          onClick={refreshToken}
-          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-        >
-          Refresh Auth Token
-        </button>
-      </div>
     </div>
   );
 }
