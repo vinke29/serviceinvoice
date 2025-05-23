5a92b9b1b359992f800ceae40d85dd63b581e4cd
Ignacio Vinke <ignaciovinke@gmail.com>
2025-05-23 18:51:37 -0400

    UI: Move business details block to bottom of invoice creation email for consistency

--- functions/index.js
+++ functions/index.js
@@ -153,11 +153,6 @@ exports.sendInvoiceEmail = functions.firestore
                     </td>
                   </tr>
                   <tr>
-                    <td style="padding:32px 40px 0 40px;">
-                      ${businessDetailsHtml}
-                    </td>
-                  </tr>
-                  <tr>
                     <td style="padding:24px 40px 0 40px;">
                       <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background:#f8fafc;border-radius:4px;">
                         <tr>
@@ -234,6 +229,7 @@ exports.sendInvoiceEmail = functions.firestore
               </td>
             </tr>
           </table>
+          ${businessDetailsHtml}
         </body>
         </html>
       `;
