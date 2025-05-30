63db52c6303c9d8528d84b9f70d5ac6de9024a55
ekn1v <lmvinke@gmail.com>
2025-05-29 23:21:28 -0400

    fix: update arcade demos for mobile tab view

--- src/components/landing/Arcade-DashboardDemo.jsx
+++ src/components/landing/Arcade-DashboardDemo.jsx
@@ -1,9 +1,11 @@
+import { useEffect, useRef } from 'react'
+
 export function DashboardDemo() {
     return (
-      <div style={{ position: 'relative', paddingBottom: 'calc(53.63204344874406% + 6px)', height: 0, width: '100%' }}>
+      <div style={{ position: 'relative', paddingBottom: 'calc(53.63204344874406% + 41px)', height: 0, width: '100%' }}>
         <iframe
-          src="https://demo.arcade.software/d2TiBnlAV1yMwW2OM6XV?embed&embed_mobile=inline&embed_desktop=inline&show_copy_link=true"
-          title="BillieNow Dashboard"
+          src="https://demo.arcade.software/dV6r1qSthRNXtVIkvyQU?embed&embed_mobile=tab&embed_desktop=inline&show_copy_link=true"
+          title="BillieNow - dashboard"
           frameBorder="0"
           loading="lazy"
           allowFullScreen
