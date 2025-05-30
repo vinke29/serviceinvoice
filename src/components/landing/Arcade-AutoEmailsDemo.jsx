6865511ada7a98d801434c8ed75d4434fe8f981e
ekn1v <lmvinke@gmail.com>
2025-05-29 23:24:02 -0400

    refactor: update arcade demo components with correct URLs and titles

--- src/components/landing/Arcade-AutoEmailsDemo.jsx
+++ src/components/landing/Arcade-AutoEmailsDemo.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useRef } from 'react'
 
-export function AutoEmailsDemo() {
+export function ArcadeEmbed() {
   return (
     <div style={{ position: 'relative', paddingBottom: 'calc(53.63204344874406% + 41px)', height: 0, width: '100%' }}>
       <iframe
-        src="https://demo.arcade.software/Jeudl8dQL4IpPW37AFZ5?embed&embed_mobile=tab&embed_desktop=inline&show_copy_link=true"
-        title="BillieNow AI Automation"
+        src="https://demo.arcade.software/dV6r1qSthRNXtVIkvyQU?embed&embed_mobile=tab&embed_desktop=inline&show_copy_link=true"
+        title="BillieNow - invoice creation"
         frameBorder="0"
         loading="lazy"
         allowFullScreen
